88af28e3b7b7429ed0ba1fe182d365b98c1bfc09
kbwoodall <kerry.woodall@gmail.com>
2023-01-30 09:55:49 -0800

    2 files modified

--- pages/index.js
+++ pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
       <main className={styles.main}>
         <button onClick={fetchTodos}>Get todos</button>
         <h1 className={styles.title}>
-          Welcome to {todos.name} <a href="https://nextjs.org">Next.js!</a>
+          Welcome {todos.name} <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
